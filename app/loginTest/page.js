3e296d48fcff62c0e2685d1ee48193703c2944ac
Bueggi <christopher.buecklein@googlemail.com>
2024-05-10 19:17:40 +0200

    activated middleware

--- app/loginTest/page.js
+++ app/loginTest/page.js
@@ -7,7 +7,7 @@ const LoginTest = () => {
 
   return (
     <button
-      type="button"
+      type="button" 
       onClick={signIn}
       className="rounded bg-indigo-600 px-2 py-1 text-xs font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
     >
