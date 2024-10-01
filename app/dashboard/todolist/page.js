c2f3bf99f9a472186720ea3e613042ef0bcbcbb5
Bueggi <christopher.buecklein@googlemail.com>
2024-10-01 11:09:48 +0200

    changed nothing

---
+++ app/dashboard/todolist/page.js
@@ -0,0 +1,11 @@
+import KanBanBoard from "../../../components/dashboard/todolist/KanBanBoard";
+
+const ToDoList = () => {
+  return (
+    <>
+      <KanBanBoard></KanBanBoard>
+    </>
+  );
+};
+
+export default ToDoList;
---
+++ components/dashboard/todolist/Task.js
@@ -0,0 +1,33 @@
+import { Draggable } from "react-beautiful-dnd";
+
+const bgColorChange = (props) => {
+  return props.isDragging
+    ? "lightgreen"
+    : props.isDraggable
+    ? props.isBacklog
+      ? "#F2D7D5"
+      : "#DCDCDC"
+    : props.isBacklog
+    ? "#F2D7D5"
+    : "#fffada";
+};
+
+const Task = ({ task, index }) => {
+  return (
+    <Draggable draggableId={`${task.id}`} key={task.id} index={index}>
+      {(provided, snapshot) => {
+        <div
+          {...provided.dragHandleProps}
+          {...provided.dragHandleProps}
+          ref={provided.innerRef}
+          isDragging={snapshot.isDragging}
+        ></div>
+        <div>{task.id}</div>
+        <div>{task.title}</div>
+        {provided.placeholder}
+      }}
+    </Draggable>
+  );
+};
+
+export default Task;
